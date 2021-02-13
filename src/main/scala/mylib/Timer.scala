fb17c9a685f79d9a35ff843c526e20b328ce4e5c
Ed Vidal Jr <devel@mypi3-20.attlocal.net>
2021-02-12 19:40:28 -0700

    Timer not converting yet PasCal++ @volatile-static posted https://github.com/SpinalHDL/SpinalHDL/blob/03fff1cf1e4d4fc42149e72cf2d2c8e152ba7d8d/lib/src/main/scala/spinal/lib/com/uart/UartCtrl.scala#L293

---
+++ src/main/scala/mylib/Timer.scala
@@ -0,0 +1,42 @@
+package mylib
+
+import spinal.core._
+
+class Timer(width:Int) extends Component {
+
+  /**
+    * This is the component definition that corresponds to
+    * the VHDL entity of the component
+    */
+  val io = new Bundle {
+    val tick = in Bool
+    val clear = in Bool
+    val limit = in UInt(width bits)
+    val full = in Bool
+  }
+
+  val counter = Reg(UInt(width bits))
+    when(io.tick && !io.full) {
+      counter:= counter + 1
+  }
+
+  when (io.clear) {
+    counter := 0
+  }
+
+  io.full := counter === io.limit
+}
+
+object TimerVhdl {
+  // Let's go
+  def main(args: Array[String]) {
+    SpinalVhdl(new Timer(8))
+  }
+}
+
+object TimerVerilog {
+  // Let's go
+  def main(args: Array[String]) {
+    SpinalVerilog(new Timer(8))
+  }
+}
