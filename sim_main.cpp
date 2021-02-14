07b9ba2328e959351c8a22ab4ef371b70727b7b9
Ed Vidal Jr <devel@mypi3-20.attlocal.net>
2021-02-14 15:45:31 -0700

    tb->opentrace("MyTopLevel.vcd"); - +	tb->m_core->reset = 0;  	for(unsigned clocks=0;  		clocks < 1000;  		 clocks++) { - +		if (clocks==10) tb->m_core->reset = 1;  		tb->tick();

--- sim_main.cpp
+++ sim_main.cpp
@@ -47,11 +47,11 @@ int	main(int argc, char **argv) {
 
 
 	tb->opentrace("MyTopLevel.vcd");
-	
+	tb->m_core->reset = 0;
 	for(unsigned clocks=0;
 		clocks < 1000;
 		 clocks++) {
-
+		if (clocks==10) tb->m_core->reset = 1;
 		tb->tick();
 		
 	}
