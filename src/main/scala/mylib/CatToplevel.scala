5b9710090423ddac1464ccadbc847dcd04962fca
Ed Vidal Jr <devel@mypi3-20.attlocal.net>
2021-02-23 09:49:24 -0700

    Needs the following added to CatTopLevel.v before replace SB_PLL40_CORE with SB_PLL40_CORE  clkCtrl_pll ( This is whare the
    .FEEDBACK_PATH("SIMPLE"),
    		.DIVR(4'b0100),		// DIVR =  4
    		.DIVQ(7'b0011111),		// DIVQ =  31
    		.DIVF(3'b100),		// DIVF =  4
    		.FILTER_RANGE(3'b010)	// FILTER_RANGE = 2
    icepll -i 100 -o 40 -f 40pll.h
    
    F_PLLIN:   100.000 MHz (given)
    F_PLLOUT:   40.000 MHz (requested)
    F_PLLOUT:   40.000 MHz (achieved)
    
    FEEDBACK: SIMPLE
    F_PFD:   20.000 MHz
    F_VCO:  640.000 MHz
    
    DIVR:  4 (4'b0100)
    DIVF: 31 (7'b0011111)
    DIVQ:  4 (3'b100)
    
    FILTER_RANGE: 2 (3'b010)
    
    PLL configuration written to: 40pll.h
    
    yosys -l simple.log -p 'synth_ice40 -blif CatTopLevel.blif -json CatTopLevel.json' CatTopLevel.v
    
    === CatTopLevel ===
    
       Number of wires:                 18
       Number of wire bits:             30
       Number of public wires:          18
       Number of public wire bits:      30
       Number of memories:               0
       Number of memory bits:            0
       Number of processes:              0
       Number of cells:                 14
         SB_CARRY                        2
         SB_DFFR                         4
         SB_DFFS                         2
         SB_LUT4                         5
         SB_PLL40_CORE                   1
    
    SB_PLL40_CORE #(
    		.FEEDBACK_PATH("SIMPLE"),
    		.DELAY_ADJUSTMENT_MODE_FEEDBACK("FIXED"),
    		.DELAY_ADJUSTMENT_MODE_RELATIVE("FIXED"),
    		.PLLOUT_SELECT("GENCLK"),
    		.FDA_FEEDBACK(4'b1111),
    		.FDA_RELATIVE(4'b1111),
    		.DIVR(4'b0100),		// DIVR =  4
    		.DIVQ(7'b0011111),		// DIVQ =  31
    		.DIVF(3'b100),		// DIVF =  4
    		.FILTER_RANGE(3'b010)	// FILTER_RANGE = 2
    	)

--- src/main/scala/mylib/CatToplevel.scala
+++ src/main/scala/mylib/CatToplevel.scala
@@ -3,16 +3,16 @@ package mylib
 import spinal.core._
 import spinal.lib._
 
-class PLL extends BlackBox{
+class SB_PLL40_CORE extends BlackBox{
   val io = new Bundle{
-    val clkIn    = in Bool
-    val clkOut   = out Bool
-    val isLocked = out Bool
+    val REFERENCECLK    = in Bool
+    val PLLOUTCORE   = out Bool
+    val LOCK = out Bool
   }
 
   noIoPrefix()
 }
- 
+
 
 class CatTopLevel extends Component{
   val io = new Bundle {
@@ -23,9 +23,9 @@ class CatTopLevel extends Component{
 
   // Create an Area to manage all clocks and reset things
   val clkCtrl = new Area {
-    //Instanciate and drive the PLL
-    val pll = new PLL
-    pll.io.clkIn := io.clk100Mhz
+    //Instanciate and drive the SB_PLL40_CORE
+    val pll = new SB_PLL40_CORE
+    pll.io.REFERENCECLK := io.clk100Mhz
 
     //Create a new clock domain named 'core'
     val coreClockDomain = ClockDomain.internal(
@@ -34,9 +34,9 @@ class CatTopLevel extends Component{
     )                                      // by coreClockDomain users to do some calculations
 
     //Drive clock and reset signals of the coreClockDomain previously created
-    coreClockDomain.clock := pll.io.clkOut
+    coreClockDomain.clock := pll.io.PLLOUTCORE
     coreClockDomain.reset := ResetCtrl.asyncAssertSyncDeassert(
-      input =  ! pll.io.isLocked,
+      input =  ! pll.io.LOCK,
       clockDomain = coreClockDomain
     )
   }
