c0b9a8880aa6d1b166b1ff5630345061db01c57b
Ed Vidal Jr <devel@mypi3-20.attlocal.net>
2021-03-03 17:37:55 -0700

    Testing the wrapper which Analyzing design hierarchy.. Top module:  \XmitTestTop Used module:     \UartCtrlUsageExample Used module:         \UartCtrl Used module:             \UartCtrlRx Used module:                 \BufferCC Used module:             \UartCtrlTx Used module:     \pwruprst Simulation okay === XmitTestTop ===
       Number of wires:                194
       Number of wire bits:            471
       Number of public wires:         194
       Number of public wire bits:     471
       Number of memories:               0
       Number of memory bits:            0
       Number of processes:              0
       Number of cells:                211
         SB_CARRY                       45
         SB_DFF                          3
         SB_DFFE                         7
         SB_DFFER                        8
         SB_DFFES                        5
         SB_DFFR                        40
         SB_DFFS                         3
         SB_LUT4                       100
    icetime -d hx8k -c 100 XmitTestTop.asc
    // Reading input .asc file..
    // Reading 8k chipdb file..
    // Creating timing netlist..
    // Timing estimate: 6.73 ns (148.64 MHz)
    // Checking 10.00 ns (100.00 MHz) clock constraint: PASSED.

---
+++ xmit55/sim_main.cpp
@@ -0,0 +1,68 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// Filename: 	helloworld_tb.cpp
+//
+// Project:	Verilog Tutorial Example file
+//
+// Purpose:	To demonstrate a Verilog main() program that calls a local
+//		serial port co-simulator.
+//
+// Creator:	Dan Gisselquist, Ph.D.
+//		Gisselquist Technology, LLC
+//
+////////////////////////////////////////////////////////////////////////////////
+//
+// Written and distributed by Gisselquist Technology, LLC
+//
+// This program is hereby granted to the public domain.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTIBILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.
+//
+////////////////////////////////////////////////////////////////////////////////
+//
+//
+#include <verilatedos.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <sys/types.h>
+#include <signal.h>
+#include "verilated.h"
+#include "VXmitTestTop.h"
+#include "testb.h"
+
+
+int	main(int argc, char **argv) {
+	Verilated::commandArgs(argc, argv);
+	TESTB<VXmitTestTop>	*tb
+		= new TESTB<VXmitTestTop>;
+
+	Verilated::traceEverOn(true);    // Verilator must compute traced signals
+    VL_PRINTF("Enabling waves...\n");
+
+
+
+	tb->opentrace("XmitTestTop.vcd");
+	//tb->m_core->reset = 0;
+	for(unsigned clocks=0;clocks < 100000;clocks++) {
+		
+		//if (clocks==10) tb->m_core->reset = 1;
+		//if (clocks==12)	tb->m_core->reset = 0;
+		//if (clocks==30) tb->m_core->io_leds = 65;
+		//if (clocks==31) tb->m_core->io_write_valid;
+		
+		//if (clocks==100) tb->m_core->io_switchs = 68;
+		//if (clocks==301) tb->m_core->io_write_valid;
+		
+		
+		
+		tb->tick();
+		
+	}
+	
+	printf("\n\nSimulation complete\n");
+}
